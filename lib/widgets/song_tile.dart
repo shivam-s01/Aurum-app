e452e1a5ccdc66d55d9c4929709b0da0f46bdf9a
shivam-s01 <sharmashivam9109@gmail.com>
2026-06-14 13:11:22 +0530

    fix: add missing flutter/services import for HapticFeedback

--- lib/widgets/song_tile.dart
+++ lib/widgets/song_tile.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import '../models/song.dart';
 import '../providers/player_provider.dart';
