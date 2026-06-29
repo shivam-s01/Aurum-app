cf1900c98d862e05610fbca9adddb28c6166b037
shivam-s01 <sharmashivam9109@gmail.com>
2026-06-30 02:05:44 +0530

    fix: remove just_audio_equalizer import from handler

--- lib/services/audio_handler.dart
+++ lib/services/audio_handler.dart
@@ -71,7 +71,6 @@ import 'package:flutter/foundation.dart';
 import 'package:audio_service/audio_service.dart';
 import 'package:audio_session/audio_session.dart';
 import 'package:just_audio/just_audio.dart';
-import 'package:just_audio_equalizer/just_audio_equalizer.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../models/song.dart';
